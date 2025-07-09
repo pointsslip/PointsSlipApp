2c4dc3c6ccc050d71b698272da13f079963d64be
Ari Cummings <ari@aricummings.com>
2025-07-08 23:14:35 -0400

    add a lot

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,6 @@
+using System;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace PointsSlip;
 
@@ -8,4 +10,36 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    public void calculate()
+    {
+        double pages = (double)pagesIn.Value!;
+        double pagesbonusnum = pages / 50;
+        double pagesbonus = Math.Round(pagesbonusnum, 0) * 25;
+        double pagepoints = pagesbonus + (pages * 10);
+
+        double lectures = (double)lecturesIn.Value!;
+        lectures = lectures * 5;
+
+        double passcheckout = (double)passcheckoutIn.Value!;
+        passcheckout = passcheckout + (double)givecheckoutIn.Value!;
+        double passcheckoutpoints = passcheckout * 3;
+        double findmu = (double)findmuIn.Value!;
+        double findmupoints = findmu * 5;
+        double checkoutdemo = (double)checkoutdemoIn.Value!;
+        double checkoutdemopoints = checkoutdemo * 3;
+        
+        
+        
+        
+        
+        double output = pagepoints + lectures + passcheckoutpoints + findmupoints + checkoutdemopoints;
+        display.Content = output.ToString();
+    }
+
+    public void calculateButton(object sender, RoutedEventArgs e)
+    {
+        calculate();
+    }
+    
 }
