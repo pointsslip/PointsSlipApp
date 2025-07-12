16079e34547defd0af9ee5a04143f0d2105cf491
Andrew Cummings <andrew@artistechendeavors.com>
2025-07-12 12:25:17 -0400

    Finish macos install

--- Certs/Apple/Notarize.sh
+++ Certs/Apple/Notarize.sh
@@ -1,35 +1,39 @@
-#!/bin/zsh
+#!/bin/sh
 
-## usage sh Notarize.sh "devemail" "Password1." "group.com.company" "DEVID" "./filename.zip"
-## dev_account $1 "devemail"
-## dev_Password $2 "Password1."
-## GroupID $3 "group.com.company" 
-## dev_team $4 DEVID
-## FileName $5 ./filename.zip
+###########################################################################################################
+###########################################################################################################
+###
+### Copyright 2025, The Lackner Group, Inc.
+###
+### Purpose:        Submits for notarization & 'staples' each of the files, places notarized copy
+###                 in the '_ Signed' folder.
+###
+### Usage & Notes:  This assumes a valid Developer ID certificate is installed, and the Packages have
+###                 all been compiled using that Developer ID Installer cert.
+##                 
+### Revisions:      2024.12.20 - Andrew Cummings; Created.
+###
+###########################################################################################################
+###########################################################################################################
+
+# Setup variables
+appleID="andrew@artistechendeavors.com"
+password="voqy-gjtn-qqzv-jnap"
+teamID="XLA5QQ65S8"
+
+echo
+echo "************************************************************"
+echo
 
 
-#xcrun altool --notarize-app --primary-bundle-id "$3" --username $1 --password "$2" --asc-provider "$4" --file "$5"
-requestUUID=$(xcrun altool --notarize-app --primary-bundle-id "$3" --username $1 --password "$2" --asc-provider "$4" --file "$5">&1  | awk '/RequestUUID/ { print $NF; }')
-
-echo "Notarization RequestUUID: $requestUUID"
-    
-if [[ $requestUUID == "" ]]; then 
-    echo "could not upload for notarization"
-    exit 1
-fi
-
-# wait for status to be not "in progress" any more
-request_status="in progress"
-while [[ "$request_status" == "in progress" ]]; do
-    echo -n "waiting... "
-    sleep 10
-    request_status=$(xcrun altool --notarization-info "$requestUUID" --username $1 --password "$2"  2>&1  | awk -F ': ' '/Status:/ { print $2; }' )
-    echo "$request_status"
-done
-
-xcrun altool --notarization-info "$requestUUID" --username $1 --password "$2"
-    
-if [[ $request_status != "success" ]]; then
-    echo "## could not notarize $filepath"
-    exit 1
-fi
+# For each set of files... we'll assume they each been newly built.  Copy to signed folder, then sign.
+src="/Users/hauchmahler/Dropbox/Mac/Documents/GitHub/PointsSlipApp/bin/osx/PointsSlip_osx.dmg"
+dst="/Users/hauchmahler/Dropbox/Mac/Documents/GitHub/PointsSlipApp/bin/osx/PointsSlip_osx_Notarized.dmg"
+mv ${src} ${dst}
+echo BEGIN: $dst
+xcrun notarytool submit "${dst}" --apple-id "${appleID}" --password "${password}" --team-id "${teamID}" --wait
+xcrun stapler staple "${dst}"
+echo DONE: $dst
+echo
+echo "************************************************************"
+echo
--- Certs/Apple/publish.sh
+++ Certs/Apple/publish.sh
@@ -1,10 +1,46 @@
+mkdir bin
 dotnet publish PointsSlip.csproj -c Release -r osx-x64 -p:UseAppHost=true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:IncludeAllContentForSelfExtract=true --self-contained true -p:PublishReadyToRunShowWarnings=true -o bin/osx-x64
+dotnet publish PointsSlip.csproj -c Release -r osx-arm64 -p:UseAppHost=true -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=true -p:IncludeAllContentForSelfExtract=true --self-contained true -p:PublishReadyToRunShowWarnings=true -o bin/osx-arm64
 
 rm bin/osx-x64/*.pdb
+rm bin/osx-arm64/*.pdb
+chmod +x bin/osx-arm64/PointsSlip
 chmod +x bin/osx-x64/PointsSlip
+mkdir bin/osx
+lipo -create -output bin/osx/PointsSlip bin/osx-x64/PointsSlip bin/osx-arm64/PointsSlip
+rm -fr bin/osx-x64
+rm -fr bin/osx-arm64
 
-codesign --remove-signature bin/osx-x64/PointsSlip
+codesign --remove-signature bin/osx/PointsSlip
 
-cp Assets/Templates/PointsSlip.app bin/osx-x64/PointsSlip.app
-cp bin/osx-x64/PointsSlip bin/osx-x64/PointsSlip.app/Contents/MacOS/PointsSlip
-cp Assets/Logos/logo.icns bin/osx-x64/PointsSlip.app/Contents/Resources/logo.icns
+cp -R Assets/Templates/PointsSlip.app/ bin/osx/PointsSlip.app/
+cp bin/osx/PointsSlip bin/osx/PointsSlip.app/Contents/MacOS/PointsSlip
+cp Assets/Logos/logo.icns bin/osx/PointsSlip.app/Contents/Resources/logo.icns
+
+rm bin/osx/PointsSlip
+rm -fr bin/Release
+
+mkdir bin/osx/PointsSlip
+mv bin/osx/PointsSlip.app bin/osx/PointsSlip/PointsSlip.app
+
+codesign --remove-signature bin/osx/PointsSlip/PointsSlip.app
+
+codesign --deep --timestamp --force --verify --verbose --sign "Developer ID Application: Andrew Cummings (XLA5QQ65S8)" bin/osx/PointsSlip/PointsSlip.app
+
+codesign -vvv --deep --strict --verbose bin/osx/PointsSlip/PointsSlip.app
+
+ln -s /Applications bin/osx/PointsSlip/Applications
+
+create-dmg bin/osx/PointsSlip_osx.dmg bin/osx/PointsSlip/ --volname "Points Slip" --volicon /Users/hauchmahler/Dropbox/Mac/Documents/GitHub/PointsSlipApp/Assets/Logos/logo.icns --filesystem APFS --window-size 300 500 --background /Users/hauchmahler/Dropbox/Mac/Documents/GitHub/PointsSlipApp/Assets/Images/background.png
+
+codesign --remove-signature bin/osx/PointsSlip_osx.dmg
+
+codesign --deep --timestamp --force --verify --verbose --sign "Developer ID Application: Andrew Cummings (XLA5QQ65S8)" bin/osx/PointsSlip_osx.dmg
+
+codesign -vvv --deep --strict --verbose bin/osx/PointsSlip_osx.dmg
+
+sh Certs/Apple/Notarize.sh
+
+codesign -vvv --deep --strict --verbose bin/osx/PointsSlip_osx_Notarized.dmg
+
+mv bin/osx/PointsSlip_osx_Notarized.dmg bin/osx/PointsSlip_osx.dmg
